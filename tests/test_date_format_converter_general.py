18bc97773d3d35559c28ec12a631c3f3aa83b6c0
Chenyang Li <cli049@uottawa.ca>
2025-04-11 21:24:45 -0400

    Added test_date_format_converter_general.py

---
+++ tests/test_date_format_converter_general.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+
+
+def test_run_script():
+    """
+    Test the script execution with simulated user input.
+    """
+    current_dir = os.path.dirname(__file__)
+    script_path = os.path.join(current_dir, "..", "src", "date_format_converter.py")
+    out = subprocess.run(
+        ["python3", script_path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+    assert (
+        out.returncode == 0
+    ), f"Script failed with return code {out.returncode} and stderr: {out.stderr}"
